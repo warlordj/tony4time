cc5f96bf8b0a7edc72a1e7012bc0da8453654119
warlordj <jeremy@keenpayroll.ca>
2025-01-10 13:09:30 +0000

    Revert "added api gateway"
    This reverts commit 51625db917b25bf7fe8e6eb04a7ba8575ed1b04b.

--- infra/api.ts
+++ infra/api.ts
@@ -1,6 +1,7 @@
-export const api = new sst.aws.ApiGatewayV2( "MyApi" );
+import { bucket } from "./storage";
 
-api.route("GET /", {
-  handler: "packages/functions/src/api.handler",
-  runtime: "nodejs20.x",
+export const myApi = new sst.aws.Function("MyApi", {
+  url: true,
+  link: [bucket],
+  handler: "packages/functions/src/api.handler"
 });
