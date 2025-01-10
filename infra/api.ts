fd01c9b63dc94ab183eee6b0a1f7158c17b59569
Jeremy Lee <jeremy@keenpayroll.ca>
2025-01-10 10:17:54 -0330

    slowly adding lambda

--- infra/api.ts
+++ infra/api.ts
@@ -3,5 +3,9 @@
 //   handler: "packages/functions/src/api.handler"
 // });
 
-
 export const myApi = new sst.aws.ApiGatewayV2("MyApi");
+
+myApi.route("GET /notes", {
+  handler: "packages/functions/src/api.handler",
+  runtime: "nodejs20.x",
+});
